ced1323b6541dc1c46383dde41333653837e37d7
SirRainman <810061281@qq.com>
2020-05-19 22:55:07 +0800

    啊啊啊

---
+++ Thread/src/main/java/com/rain/test/Test.java
@@ -0,0 +1,28 @@
+package com.rain.test;
+
+public class Test {
+    private static int count = 0;
+    private static Object lock = new Object();
+
+    public static void main(String[] args) {
+
+
+        new Thread(()->{
+            for (int j = 0; j < 1000; j++) {
+                synchronized (lock) {
+                    count++;
+                }
+            }
+        }).start();
+
+        new Thread(()->{
+            for (int j = 0; j < 1000; j++) {
+                synchronized (lock) {
+                    count--;
+                }
+            }
+        }).start();
+
+        System.out.println(count);
+    }
+}
